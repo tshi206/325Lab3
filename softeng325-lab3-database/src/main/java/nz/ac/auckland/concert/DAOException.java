87dc8fe77dc4c9ab863fda233cc7d18d3e4598e6
tshi206 <tshi206@aucklanduni.ac.nz>
2017-08-14 21:49:24 +1200

    Task4

---
+++ softeng325-lab3-database/src/main/java/nz/ac/auckland/concert/DAOException.java
@@ -0,0 +1,12 @@
+package nz.ac.auckland.concert;
+
+public class DAOException extends Exception {
+
+	public DAOException() {
+		super();
+	}
+	
+	public DAOException(String message) {
+		super(message);
+	}
+}
